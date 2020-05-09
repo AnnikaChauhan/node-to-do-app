d50f69fd67b182f76e4bb9f0b6ac3f3cc5aa1134
Annika Chauhan <annikac0711@gmail.com>
2020-05-09 13:22:59 +0100

    connected to a mongoDB database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,22 @@
 let data = [{item: 'Get Milk'}, {item: 'Walk Dog'}, {item: 'Code'}];
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb+srv://test:test@cluster0-cadcf.gcp.mongodb.net/test?retryWrites=true&w=majority');
+
+// create schema - this is like a blueprint for data
+var todoSchema = new mongoose.Schema({
+    item: String
+})
+
+// create a todo model, with the model name and the schema it is based on
+
+var Todo = mongoose.model('Todo', todoSchema);
+//test item created of type Todo, once you do nodemon app it will push this into the database
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if(err) throw err;
+    console.log('item saved');
+})
 
 module.exports = function(app){
 
