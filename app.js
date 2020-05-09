91b41440d27a295473ef66c2a6f50bf10a3842e3
Annika Chauhan <annikac0711@gmail.com>
2020-05-09 09:44:11 +0100

    controller for to do added, only one controller for this app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const todoController = require('./controllers/todoController');
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -7,6 +8,9 @@ app.set('view engine', 'ejs');
 
 //static files for middleware
 app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
 
 app.listen(3000);
 console.log('listening to port 3000');
